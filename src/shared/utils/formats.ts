df821a6ef0f97e376bf168c8990bbcadbb79461e
manuel <ppjuan_94@hotmail.com>
2025-09-05 13:35:47 -0600

    fix: corrige formato de fecha
    Corrige el formato de la fecha para mostrar el mes abreviado.
    
    - Se modifica el formato de fecha para usar "MMM" en lugar de "MMMM", abreviando el nombre del mes.

--- src/shared/utils/formats.ts
+++ src/shared/utils/formats.ts
@@ -18,7 +18,7 @@ export const formatCurrency = (
 
 export const formatDate = (date?: Date | string | number) => {
   if (!date) return "0000 0, 0000";
-  return format(new Date(date), "MMMM d, yyyy", {
+  return format(new Date(date), "MMM d, yyyy", {
     locale: enUS,
   });
 };
