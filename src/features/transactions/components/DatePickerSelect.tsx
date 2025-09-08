e57e41c578c2ab037dd713b550ee9be3cf1b08f5
manuel <ppjuan_94@hotmail.com>
2025-09-08 17:40:17 -0600

    fix: sincroniza la fecha seleccionada
    Corrige el problema de desincronización entre el valor del DatePicker y el estado interno.
    
    - Asegura que el componente DatePickerSelect refleje correctamente la fecha proporcionada externamente.
    - Permite actualizar el estado interno del DatePicker cuando la prop 'value' cambia.
    - Implementa la función onChange para propagar los cambios de fecha al componente padre.

--- src/features/transactions/components/DatePickerSelect.tsx
+++ src/features/transactions/components/DatePickerSelect.tsx
@@ -2,15 +2,29 @@ import { DatePicker, formatDate } from "@/shared";
 import { Button, Menu, MenuButton, MenuList, Portal } from "@chakra-ui/react";
 import { IconCalendar } from "@tabler/icons-react";
 import { isValid } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
   defaultValue: Date;
-  value: Date;
+  value: Date | null;
+  onChange?: (date: Date | null) => void;
 };
 
-export default function DatePickerSelect({ defaultValue, value }: Props) {
+export default function DatePickerSelect({
+  defaultValue,
+  value,
+  onChange,
+}: Props) {
   const [date, setDate] = useState<Date | null>(value);
+
+  useEffect(() => {
+    setDate(value);
+  }, [value]);
+
+  const handleDateChange = (newDate: Date | null) => {
+    setDate(newDate);
+    onChange?.(newDate);
+  };
 
   const dateFormat = isValid(date) ? formatDate(date as Date) : "Unkwnow";
 
@@ -36,9 +50,9 @@ export default function DatePickerSelect({ defaultValue, value }: Props) {
       <Portal>
         <MenuList>
           <DatePicker
-            onChange={setDate}
+            onChange={handleDateChange}
             defaultValue={defaultValue}
-            value={value}
+            value={date}
           />
         </MenuList>
       </Portal>
