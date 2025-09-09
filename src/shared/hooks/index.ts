e6ebdef4104fe9d1ec54331a5615e7d6054d9741
manuel <ppjuan_94@hotmail.com>
2025-09-09 17:49:50 -0600

    refactor: elimina hooks sin usar
    Elimina varios hooks que ya no se utilizan en el proyecto para reducir el tamaño del paquete y simplificar el código.
    
    - Elimina los hooks useBoolean, useDisclosure y useFileHandler.
    - Elimina las exportaciones correspondientes en el archivo index.ts.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,7 +1,4 @@
-export { default as useBoolean } from "./useBoolean";
-export { default as useDisclosure } from "./useDisclosure";
 export { default as useQueryParams } from "./useQueryParams";
-export { default as useFileHandler } from "./useFileHandler";
 export { default as useAuthenticatedUser } from "./useAuthenticatedUser";
 export { default as usePagination } from "./usePagination";
 
