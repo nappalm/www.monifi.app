64de0624c1e4f44c532f9093a35ccc24f9ff1d2e
manuel <ppjuan_94@hotmail.com>
2026-03-02 15:58:20 -0600

    refactor: actualiza variable de entorno para api url
    actualiza la variable de entorno utilizada para definir la url base de la api.
    - unifica el nombre de la variable de entorno en todas las referencias de configuración.
    esto mejora la consistencia y claridad al configurar el cliente axios.

--- src/lib/axios/mainApiClient.ts
+++ src/lib/axios/mainApiClient.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const getMainApiUrl = (path: string) =>
-  import.meta.env.VITE_API_BASE_URL + path;
+  import.meta.env.VITE_APP_API_URL + path;
 
 export const mainApiClient = Axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
